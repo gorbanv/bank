b3a4c9f888c5bb6110b969c41de1e3b6bb041517
VladimirGorban <energet164@gmail.com>
2022-10-08 17:37:32 +0300

    update Currency class, add methods;

--- App/Currency/Currency.php
+++ App/Currency/Currency.php
@@ -1,11 +1,46 @@
 <?php
 namespace App\Currency;
 
+use App\Currency\CurrencyData;
+use App\Converter\CurrencyConverter;
+
 abstract class Currency
 {
-    private float $amount;
+    private $amount;
 
-    public function __construct() {}
-    public function getBalance() {}
-    public function setBalance() {}
+    public function __construct($amount = 0) {
+        $this->amount = 0;
+        if (is_numeric($amount)) {
+            $this->amount = $amount;
+        }
+        else if ($amount instanceof Currency) {
+            $this->amount += $this->convertCurrency($amount);
+        }
+    }
+
+    public function getBalance() {
+        return $this->amount;
+    }
+    public function setBalance(float $amount) {
+        $this->amount = $amount;
+    }
+
+    public function convertCurrency(Currency $currency){
+        $converter = new CurrencyConverter();
+        $exchangeData = CurrencyData::getExchangeRate(static::CURRENCY_NAME);
+        $converter->prepareConverter($currency::CURRENCY_NAME);
+
+        return $converter->instance->convert(
+            static::CURRENCY_NAME,
+            $currency->getBalance(),
+            $exchangeData[CurrencyData::RUB]
+        );
+    }
+
+    /* public static function createFromCurrency(Currency $currency) {
+    } */
+
+    public static function setExchangeRate(String $exchangeCurrency, float $exchangeRate) {
+        CurrencyData::setExchangeRate(static::CURRENCY_NAME, $exchangeCurrency, $exchangeRate);
+    }
 }
