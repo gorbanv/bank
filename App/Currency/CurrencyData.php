b67e3481a63e045284c734f21619cbbb7e8b647e
VladimirGorban <energet164@gmail.com>
2022-10-08 14:55:00 +0300

    added class CurrencyData;

---
+++ App/Currency/CurrencyData.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Currency;
+
+class CurrencyData
+{
+    const RUB = 'RUB';
+    const EUR = 'EUR';
+    const USD = 'USD';
+
+    private static array $exchangeRate = [
+        'EUR' => [
+            'USD' => 1,
+            'RUB' => 80,
+        ],
+        'USD' => [
+            'EUR' => 1,
+            'RUB' => 70,
+        ],
+        'RUB' => [
+            'EUR' => 80,
+            'USD' => 70,
+        ]
+    ];
+
+    public static function setExchangeRate(String $mainCurrency, String $changeCurrency, float $exchangeRate) {
+        self::$exchangeRate[$mainCurrency][$changeCurrency] = $exchangeRate;
+        self::$exchangeRate[$changeCurrency][$mainCurrency] = $exchangeRate;
+    }
+    public static function getExchangeRate(String $currency) {
+        return self::$exchangeRate[$currency];
+    }
+
+}
