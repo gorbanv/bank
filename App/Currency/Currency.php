7800dde598dd097d6fc659377a272acf988fde9f
VladimirGorban <energet164@gmail.com>
2022-10-08 07:54:41 +0300

    added Currency class methods;

--- App/Currency/Currency.php
+++ App/Currency/Currency.php
@@ -3,5 +3,9 @@ namespace App\Currency;
 
 abstract class Currency
 {
+    private float $amount;
 
+    public function __construct() {}
+    public function getBalance() {}
+    public function setBalance() {}
 }
