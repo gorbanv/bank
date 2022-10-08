b893ca1b4a7eb238d9e6ae284beb1dbbc48489f5
VladimirGorban <energet164@gmail.com>
2022-10-08 17:06:35 +0300

    added update convert method for Converters;

--- App/Converter/Converters/EurConverter.php
+++ App/Converter/Converters/EurConverter.php
@@ -7,6 +7,18 @@ use App\Currency\CurrencyData;
 class EurConverter implements Converter
 {
     public function convert(string $toCurrencyName, int $amount, $rate): float {
-
+        switch ($toCurrencyName) {
+            case CurrencyData::EUR:
+                return round($amount * $rate, 2);
+                break;
+            case CurrencyData::USD:
+                return round($amount / $rate, 2);
+                break;
+            case CurrencyData::RUB:
+                return round($amount / $rate, 2);
+                break;
+            default:
+                throw new Exception('Currency for exchange not found');
+        }
     }
 }
--- App/Converter/Converters/RubConverter.php
+++ App/Converter/Converters/RubConverter.php
@@ -9,6 +9,18 @@ class RubConverter implements Converter
 
     public function convert(string $toCurrencyName, int $amount, $rate): float
     {
-
+        switch ($toCurrencyName) {
+            case CurrencyData::RUB:
+                return round($amount * $rate, 2);
+                break;
+            case CurrencyData::EUR:
+                return round($amount / $rate, 2);
+                break;
+            case CurrencyData::USD:
+                return round($amount / $rate, 2);
+                break;
+            default:
+                throw new Exception('Currency for exchange not found');
+        }
     }
 }
