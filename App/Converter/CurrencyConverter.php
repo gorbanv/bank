259604fbe35f362bf59b532b0d4e229dec62252e
VladimirGorban <energet164@gmail.com>
2022-10-08 16:44:12 +0300

    added method prepareConverter for Converter;

--- App/Converter/CurrencyConverter.php
+++ App/Converter/CurrencyConverter.php
@@ -3,14 +3,35 @@
 namespace App\Converter;
 
 use App\Converter\Converter;
-
+use App\Converter\Converters\RubConverter;
+use App\Converter\Converters\EurConverter;
+use App\Converter\Converters\UsdConverter;
 
 class CurrencyConverter
 {
+
+
     public Converter $instance;
 
     public function setConverter(Converter $converter)
     {
         $this->instance = $converter;
+    }
+
+    public function prepareConverter(string $fromName)
+    {
+        switch ($fromName) {
+            case 'RUB';
+                $this->setConverter(new RubConverter());
+                break;
+            case 'EUR';
+                $this->setConverter(new EurConverter());
+                break;
+            case 'USD';
+                $this->setConverter(new UsdConverter());
+                break;
+            default:
+                throw new Exception('Currency converter not found');
+        }
     }
 }
