13dcad2498c7f1ecbdf86b363edd500d391df713
Vladimir164 <energet164@gmail.com>
2022-10-10 09:13:54 +0300

    update code index.php;

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ namespace App;
 require_once('vendor/autoload.php');
 
 use App\Bank\Bank;
+use App\Accounts\MultiCurrencyAccount;
 use App\Currency\Currencies\{Rub, Eur, Usd};
 use App\Currency\CurrencyData;
 
@@ -23,9 +24,9 @@ $account->deposit(new USD(50));
 
 // Step 2
 echo "Step 2\n";
-echo $account->getBalance() . PHP_EOL;
-echo $account->getBalance('USD') . PHP_EOL;
-echo $account->getBalance('EUR') . PHP_EOL;
+echo $account->getBalance() . " RUB\n";
+echo $account->getBalance('USD') . " USD\n";
+echo $account->getBalance('EUR') . " EUR\n";
 
 // Step 3
 echo "Step 3\n";
@@ -40,18 +41,18 @@ Usd::setExchangeRate('RUB', 100);
 
 // Step 5
 echo "Step 5\n";
-echo $account->getBalance() . PHP_EOL;
+echo $account->getBalance() . " EUR\n";
 
 // Step 6
 echo "Step 6\n";
 $account->setDefaultCurrency('EUR');
-echo $account->getBalance() . PHP_EOL;
+echo $account->getBalance() . " EUR\n";
 
 // Step 7
 echo "Step 7\n";
 $cash = $account->withdraw(new RUB(1000));
 $account->deposit(new EUR($cash));
-echo $account->getBalance('EUR') . PHP_EOL;
+echo $account->getBalance('EUR') . " EUR\n";
 
 echo "Step 8\n";
 Eur::setExchangeRate('RUB', 120);
@@ -64,4 +65,4 @@ $account->setDefaultCurrency('RUB');
 $account->removeAccount('EUR');
 $account->removeAccount('USD');
 print_r($account->getSuppliedCurrency());
-echo $account->getBalance() . PHP_EOL;
+echo $account->getBalance() . " RUB\n";
